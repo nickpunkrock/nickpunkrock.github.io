c895e43ba135a85fc555bac0679cb28f4e5d1987
nickpunkrock <nickpunkrockanarchy@gmail.com>
2016-03-19 01:47:46 +0300

    grouo

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,15 +1,16 @@
 $(function() 
 { 
 $(".tab_content").hide(); 
-for (i=0;i<=$("ul.tabs li").length-1;i++) { 
-    if ($("ul.tabs li")[i].children.length!=0) 
-    $("ul.tabs li")[i].click(function() { 
-    $("ul.tabs li").removeClass("active"); 
-    $(this).addClass("active"); 
-    $(".tab_content").hide(); 
-    var activeTab = $(this).find("a").attr("href"); 
-    $(activeTab).fadeIn(); 
-    return false; 
+for (i=0;i<=$("ul.tabs li").length-1;i++) 
+{ 
+if ($("div.tab_container div")[i].children.length!=0) 
+$("ul.tabs li").click(function() { 
+$("ul.tabs li").removeClass("active"); 
+$(this).addClass("active"); 
+$(".tab_content").hide(); 
+var activeTab = $(this).find("a").attr("href"); 
+$(activeTab).fadeIn("slow"); 
+return false; 
 }); 
 } 
 hash = window.location.hash; 
