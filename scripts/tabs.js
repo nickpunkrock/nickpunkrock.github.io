9d63d765e06d38061ea86bb014289c56a61dcfec
nickpunkrock <nickpunkrockanarchy@gmail.com>
2016-03-19 01:38:19 +0300

    groups

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,16 +1,15 @@
 $(function() 
 { 
 $(".tab_content").hide(); 
-for (i=0;i<=$("ul.tabs li").length-1;i++) 
-{ 
-if ($("ul.tabs li")[i].children.length!=0) 
-$("ul.tabs li").click(function() { 
-$("ul.tabs li").removeClass("active"); 
-$(this).addClass("active"); 
-$(".tab_content").hide(); 
-var activeTab = $(this).find("a").attr("href"); 
-$(activeTab).fadeIn("slow"); 
-return false; 
+for (i=0;i<=$("ul.tabs li").length-1;i++) { 
+    if ($("ul.tabs li")[i].children.length!=0) 
+    $("ul.tabs li")[i].click(function() { 
+    $("ul.tabs li").removeClass("active"); 
+    $(this).addClass("active"); 
+    $(".tab_content").hide(); 
+    var activeTab = $(this).find("a").attr("href"); 
+    $(activeTab).fadeIn(); 
+    return false; 
 }); 
 } 
 hash = window.location.hash; 
