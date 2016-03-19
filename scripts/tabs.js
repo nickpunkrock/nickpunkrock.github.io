3d8d6fa302ca4e0c04b75499a5b4a4deed7c62f5
nickpunkrock <nickpunkrockanarchy@gmail.com>
2016-03-19 23:56:04 +0300

    Группа

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -4,7 +4,7 @@ $(".tab_content").hide();
 for (i=0;i<=$("ul.tabs li").length-1;i++) 
 { 
 if ($("div.tab_container div.tab_content")[i].children.length!=0)
-$("ul.tabs li").click(function() { 
+$("ul.tabs li")[i].click(function() {
 $("ul.tabs li").removeClass("active"); 
 $(this).addClass("active"); 
 $(".tab_content").hide(); 
